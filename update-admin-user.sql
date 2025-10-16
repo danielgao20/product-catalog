d740d4473e738547059df1d542ad4783eb747495
danielgao20 <dygao@usc.edu>
2025-10-16 16:28:33 -0700

    cleanup: remove unnecessary files and unused dependencies

--- update-admin-user.sql
+++
@@ -1,7 +0,0 @@
--- Update Wiley's admin user with the correct password hash for 'wiley123'
-UPDATE admin_users 
-SET password_hash = '$2b$10$k2Cgn8ijrlnYya7E.na9buuKMFjA93tHtmDCiDaLV27ZRqr.HuvFu'
-WHERE email = 'wiley@gmail.com';
-
--- Verify the update
-SELECT email, name, role, is_active FROM admin_users WHERE email = 'wiley@gmail.com';
